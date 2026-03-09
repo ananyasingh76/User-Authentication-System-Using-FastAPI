260dfb9425baef5c848d0ff3de8282aa3326cb8d
ananyasingh76 <ananyasingh23086@gmail.com>
2026-03-09 23:51:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from auth import models, schemas, utils
+from auth.auth_database import get_db
+from jose import jwt
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+
+
+SECRET_KEY = "rDYxmJMxN50gu0FFchDv6wHv5VNwiDLb5_YXCHva8tQ"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# Helper function taht takes user data
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+
+
+app = FastAPI()
+
+@app.post("/signup")
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # check the user exit or not
+    existing_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail= "Usernam already exist")
+
+    # Hash the password
+    hashed_pass = utils.hash_password(user.password)
+
+    # Create new user instance
+    new_user = models.User(
+        username = user.username,
+        email = user.email,
+        hashed_password = hashed_pass,
+        role=user.role
+    )
+
+    # Save user to database
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    # Reteurn the value (excluding password)
+    return {'id': new_user.id, "username": new_user.username, "email": new_user.email, "role": new_user.role}
+
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm =Depends(), db:Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.username == form_data.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Username")
+    if not utils.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Password")
+
+    token_data = {'sub': user.username, 'role': user.role}
+    token = create_access_token(token_data)
+    return {"access_token": token, "token_type": "bearer"}
